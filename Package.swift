684d2c996701468386856528b42503075b66a53e
Mike Silvers <mike@clearcodex.com>
2018-02-09 14:37:15 -0500

    Working on the gitignore

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2018-02-08 22:00:12 +0000
+// Date: 2018-02-09 19:17:11 +0000
 import PackageDescription
 let package = Package(
 	name: "offroaddc",
@@ -12,5 +12,7 @@ let package = Package(
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-OAuth2.git", majorVersion: 3),
 		.Package(url: "https://github.com/michaelsilvers/SwiftGD.git", majorVersion: 1),
 		.Package(url: "https://mikesilvers@bitbucket.org/clearcodex/jsonconfigenhanced.git", majorVersion: 1),
+		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Repeater.git", majorVersion: 1),
 	]
 )
