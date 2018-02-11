9302bee45891a25e198dd62341fd4c91f54aadc0
Mike Silvers <mike@clearcodex.com>
2018-02-11 12:20:35 -0500

    UPdated package

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2018-02-08 22:00:12 +0000
+// Date: 2018-02-09 22:26:46 +0000
 import PackageDescription
 let package = Package(
 	name: "offroaddc",
@@ -10,8 +10,9 @@ let package = Package(
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-LocalAuthentication-PostgreSQL.git", majorVersion: 3),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-OAuth2.git", majorVersion: 3),
-		.Package(url: "https://github.com/michaelsilvers/SwiftGD.git", majorVersion: 1),
+//		.Package(url: "https://github.com/michaelsilvers/SwiftGD.git", majorVersion: 1),
 		.Package(url: "https://mikesilvers@bitbucket.org/clearcodex/jsonconfigenhanced.git", majorVersion: 1),
-        .Package(url: "https://github.com/iamjono/SwiftMoment.git", majorVersion: 1),
+		.Package(url: "https://github.com/iamjono/SwiftMoment.git", majorVersion: 1),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Authentication.git", majorVersion: 1),
 	]
 )
