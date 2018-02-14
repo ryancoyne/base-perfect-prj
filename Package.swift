cae419a1101fe3a736a934dbb173fd1e996dacfd
Mike Silvers <mike@clearcodex.com>
2018-02-13 22:09:54 -0500

    Updated the localhost definition

--- Package.swift
+++ Package.swift
@@ -1,17 +1,17 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2018-02-09 22:26:46 +0000
+// Date: 2018-02-14 02:22:45 +0000
 import PackageDescription
 let package = Package(
 	name: "offroaddc",
 	targets: [],
 	dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Notifications.git", majorVersion: 3),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-LocalAuthentication-PostgreSQL.git", majorVersion: 3),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-OAuth2.git", majorVersion: 3),
-        .Package(url: "https://github.com/michaelsilvers/SwiftGD.git", majorVersion: 1),
-        .Package(url: "https://mikesilvers@bitbucket.org/clearcodex/jsonconfigenhanced.git", majorVersion: 1),
-        .Package(url: "https://github.com/iamjono/SwiftMoment.git", majorVersion: 1),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Notifications.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-LocalAuthentication-PostgreSQL.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-OAuth2.git", majorVersion: 3),
+		.Package(url: "https://github.com/michaelsilvers/SwiftGD.git", majorVersion: 1),
+		.Package(url: "https://mikesilvers@bitbucket.org/clearcodex/jsonconfigenhanced.git", majorVersion: 1),
+		.Package(url: "https://github.com/iamjono/SwiftMoment.git", majorVersion: 1),
 	]
 )
