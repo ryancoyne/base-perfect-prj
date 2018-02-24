362b17e91a3c8ceedd52f2af1f89d60ec701fc33
Mike Silvers <mike@clearcodex.com>
2018-02-24 07:14:54 -0500

    Working on the Stages Connector

---
+++ Sources/offroaddc/StagesConnector.swift
@@ -0,0 +1,24 @@
+//
+//  StagesConnector.swift
+//  offroaddcPackageDescription
+//
+//  Created by Mike Silvers on 2/21/18.
+//
+
+import Foundation
+
+class StagesConnecter () {
+    
+    static let sharedInstance = StagesConnecter()
+    
+    private init() {
+        
+    }
+
+    var services:[Service] = []
+    
+    func location1_classes() {
+        
+    }
+}
+
