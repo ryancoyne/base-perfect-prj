d499825af5f410a23b06d2d540414289ef8e86e8
Mike Silvers <mike@clearcodex.com>
2018-05-04 09:39:32 -0400

    Added the location type

--- Sources/offroaddc/DBTables.swift
+++ Sources/offroaddc/DBTables.swift
@@ -186,6 +186,8 @@ final class PRJDBTables {
         ClassRegistrationTable.sharedInstance.create()
         ClassStatusTable.sharedInstance.create()
         ClassTypeTable.sharedInstance.create()
+        
+        LocationTypeTable.sharedInstance.create()
 
         // make sure the tables exist.... if not - then create it
         let thereturn = CCXDBTables.sharedInstance.isPostGIS()
