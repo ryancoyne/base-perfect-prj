1f47e990949422d3399b0ca298704cbc67d2c4c3
Mike Silvers <mike.silvers@buckettechnologies.com>
2018-08-07 10:40:41 -0400

    Updating the packages and removing offroad stuff

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // Date: 2018-02-14 02:22:45 +0000
 import PackageDescription
 let package = Package(
-	name: "offroaddc",
+	name: "bucket_server",
 	targets: [],
 	dependencies: [
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
@@ -10,7 +10,7 @@ let package = Package(
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-LocalAuthentication-PostgreSQL.git", majorVersion: 3),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-OAuth2.git", majorVersion: 3),
-        .Package(url:"https://github.com/PerfectlySoft/Perfect-XML.git", majorVersion: 3),
+// .Package(url:"https://github.com/PerfectlySoft/Perfect-XML.git", majorVersion: 3),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 3),
 		.Package(url: "https://github.com/michaelsilvers/SwiftGD.git", majorVersion: 1),
 		.Package(url: "https://mikesilvers@bitbucket.org/clearcodex/jsonconfigenhanced.git", majorVersion: 1),
