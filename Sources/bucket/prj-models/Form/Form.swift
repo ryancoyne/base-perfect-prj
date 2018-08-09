8e3bd2a35615724fc95c650700f4fb1c161b17bc
Ryan <ryan@buckettechnologies.com>
2018-08-09 09:21:07 -0400

    Working on the form maodel

---
+++ Sources/bucket/prj-models/Form/Form.swift
@@ -0,0 +1,182 @@
+//
+//  Form.swift
+//  bucket
+//
+//  Created by Ryan Coyne on 8/9/18.
+//
+
+import Foundation
+import PerfectHTTP
+import StORM
+import PostgresStORM
+
+public class Form: PostgresStORM {
+    
+    // NOTE: First param in class should be the ID.
+    var id         : Int?    = nil
+    var created    : Int?    = nil
+    var createdby  : String? = nil
+    var modified   : Int?    = nil
+    var modifiedby : String? = nil
+    var deleted    : Int?    = nil
+    var deletedby  : String? = nil
+    
+    var name     : String? = nil
+    var title     : String? = nil
+    var description     : String? = nil
+    
+    //MARK: Table name
+    override public func table() -> String { return "form" }
+    
+    //MARK: Functions to retrieve data and such
+    override open func to(_ this: StORMRow) {
+        
+        if let data = this.data.id.intValue {
+            id = data
+        }
+        
+        if let data = this.data.created.intValue {
+            created = data
+        }
+        
+        if let data = this.data.modified.intValue {
+            modified = data
+        }
+        
+        if let data = this.data.deleted.intValue {
+            deleted = data
+        }
+        
+        if let data = this.data.createdBy {
+            createdby = data
+        }
+        
+        if let data = this.data.modifiedBy {
+            modifiedby = data
+        }
+        
+        if let data = this.data.deletedBy {
+            deletedby = data
+        }
+        
+        if let data = this.data.formDic.name {
+            name = data
+        }
+        
+        if let data = this.data.formDic.title {
+            title = data
+        }
+        
+        if let data = this.data.shortdescription {
+            description = data
+        }
+        
+    }
+    
+    func rows() -> [Form] {
+        var rows = [Form]()
+        for i in 0..<self.results.rows.count {
+            let row = Form()
+            row.to(self.results.rows[i])
+            rows.append(row)
+        }
+        return rows
+    }
+    
+    func fromDictionary(sourceDictionary: [String:Any]) {
+        
+        for (key, value) in sourceDictionary {
+            
+            switch key.lowercased() {
+                
+            case "name":
+                if (value as? String).isNotNil {
+                    self.name = (value as! String)
+                }
+            
+            case "title":
+                if (value as? String).isNotNil {
+                    self.title = (value as! String)
+                }
+                
+            case "description":
+                if (value as? String).isNotNil {
+                    self.description = (value as! String)
+                }
+                
+            default:
+                print("This should not occur")
+            }
+            
+        }
+        
+    }
+    
+    func asDictionary() -> [String: Any] {
+        
+        var dictionary:[String:Any] = [:]
+        
+        if self.id.isNotNil {
+            dictionary.id = self.id
+        }
+        
+        if self.created.isNotNil {
+            dictionary.created = self.created
+        }
+        
+        if self.createdby.isNotNil {
+            dictionary.createdBy = self.createdby
+        }
+        
+        if self.modified.isNotNil {
+            dictionary.modified = self.modified
+        }
+        
+        if self.modifiedby.isNotNil {
+            dictionary.modifiedBy = self.modifiedby
+        }
+        
+        if self.deleted.isNotNil {
+            dictionary.deleted = self.deleted
+        }
+        
+        if self.deletedby.isNotNil {
+            dictionary.deletedBy = self.deletedby
+        }
+        
+        if self.name.isNotNil {
+            dictionary.formDic.name = self.name
+        }
+        
+        if self.title.isNotNil {
+            dictionary.formDic.title = self.title
+        }
+        
+        if self.description.isNotNil {
+            dictionary.shortdescription = self.description
+        }
+        
+        return dictionary
+    }
+    
+    // true if they are the same, false if the target item is different than the core item
+    func compare(targetItem: Form)-> Bool {
+        
+        var diff = true
+        
+        if diff == true, self.name != targetItem.name {
+            diff = false
+        }
+        
+        if diff == true, self.title != targetItem.title {
+            diff = false
+        }
+        
+        if diff == true, self.description != targetItem.description {
+            diff = false
+        }
+        
+        return diff
+        
+    }
+}
