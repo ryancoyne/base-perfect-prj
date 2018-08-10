c787c62ac709be2b89299f8ac97153f151ef4c49
Mike Silvers <mike.silvers@buckettechnologies.com>
2018-08-10 10:15:03 -0400

    Working on the retailer handlers and code creation

--- Sources/bucket/PRJExtensions.swift
+++ Sources/bucket/PRJExtensions.swift
@@ -1,44 +1,107 @@
-extension Dictionary where Key == String, Value == Any {
-    var capsuleskin : CapsuleskinDictionary! {
-        get {
-            var bc = CapsuleskinDictionary()
-            bc.dic = self
-            return bc
-        }
-        set {
-            self = newValue.dic
-        }
+extension String {
+    /// Create new instance with random numeric/alphabetic/alphanumeric String of given length.
+    ///
+    /// - Parameters:
+    ///   - randommWithLength:      The length of the random String to create.
+    ///   - allowedCharactersType:  The allowed characters type, see enum `AllowedCharacters`.
+    public init?(randomWithLength length: Int, allowedCharactersType: AllowedCharacters) {
+        let allowedCharsString: String = {
+            switch allowedCharactersType {
+            case .numeric:
+                return "0123456789"
+                
+            case .alphabetic:
+                return "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                
+            case .alphaNumeric:
+                return "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+                
+            case .allCharactersIn(let allowedCharactersString):
+                return allowedCharactersString
+            }
+        }()
+        
+        self.init(allowedCharsString.sample(size: length)!)
+    }
+    
+    /// - Returns: `true` if contains any cahracters other than whitespace or newline characters, else `no`.
+    public var isBlank: Bool { return stripped().isEmpty }
+    
+    /// - Returns: The string stripped by whitespace and newline characters from beginning and end.
+    public func stripped() -> String { return trimmingCharacters(in: .whitespacesAndNewlines) }
+    
+    /// Returns a random character from the String.
+    ///
+    /// - Returns: A random character from the String or `nil` if empty.
+    public var sample: Character? {
+        return isEmpty ? nil : self[index(startIndex, offsetBy: Int(randomBelow: count)!)]
+    }
+    
+    /// Returns a given number of random characters from the String.
+    ///
+    /// - Parameters:
+    ///   - size: The number of random characters wanted.
+    /// - Returns: A String with the given number of random characters or `nil` if empty.
+    public func sample(size: Int) -> String? {
+        guard !isEmpty else { return nil }
+        
+        var sampleElements = String()
+        size.times { sampleElements.append(sample!) }
+        
+        return sampleElements
     }
 }
 
-//MARK: Capsuleskin Dictionary Variable Values
-struct CapsuleskinDictionary {
-    fileprivate var dic : [String:Any]!
-    /// This variable key is "description". Set nil to remove from the dictionary.
-    var capsuleskin_description : String? {
-        get {
-            return self.dic["capsuleskin_description"] as? String
-        }
-        set {
-            if newValue != nil {
-                self.dic["capsuleskin_description"] = newValue!
-            } else {
-                self.dic.removeValue(forKey: "capsuleskin_description")
-            }
-        }
-    }
-    /// This variable key is "capsuleidentifier". Set nil to remove from the dictionary.
-    var capsuleskin_identifier : Int? {
-        get {
-            return self.dic["capsuleskin_identifier"] as? Int
-        }
-        set {
-            if newValue != nil {
-                self.dic["capsuleskin_identifier"] = newValue!
-            } else {
-                self.dic.removeValue(forKey: "capsuleskin_identifier")
-            }
-        }
+extension String {
+    /// The type of allowed characters.
+    ///
+    /// - Numeric:          Allow all numbers from 0 to 9.
+    /// - Alphabetic:       Allow all alphabetic characters ignoring case.
+    /// - AlphaNumeric:     Allow both numbers and alphabetic characters ignoring case.
+    /// - AllCharactersIn:  Allow all characters appearing within the specified String.
+    public enum AllowedCharacters {
+        case numeric
+        case alphabetic
+        case alphaNumeric
+        case allCharactersIn(String)
     }
 }
 
+extension Int {
+    /// Initializes a new `Int ` instance with a random value below a given `Int`.
+    ///
+    /// - Parameters:
+    ///   - randomBelow: The upper bound value to create a random value with.
+    public init?(randomBelow upperLimit: Int) {
+        
+        
+        
+        guard upperLimit > 0 else { return nil }
+        #if os(Linux)
+        self.init(random() % upperLimit)
+        #else
+        self.init(arc4random_uniform(UInt32(upperLimit)))
+        #endif
+        
+    }
+    
+    
+    /// Runs the code passed as a closure the specified number of times.
+    ///
+    /// - Parameters:
+    ///   - closure: The code to be run multiple times.
+    public func times(_ closure: () -> Void) {
+        guard self > 0 else { return }
+        for _ in 0..<self { closure() }
+    }
+    
+    /// Runs the code passed as a closure the specified number of times
+    /// and creates an array from the return values.
+    ///
+    /// - Parameters:
+    ///   - closure: The code to deliver a return value multiple times.
+    public func timesMake<ReturnType>(_ closure: () -> ReturnType) -> [ReturnType] {
+        guard self > 0 else { return [] }
+        return (0..<self).map { _ in return closure() }
+    }
+}
--- Sources/bucket/prj-apis/RetailerAPI.swift
+++ Sources/bucket/prj-apis/RetailerAPI.swift
@@ -54,7 +54,6 @@ struct RetailerAPI {
                 request, response in
                 
                 
-                
             }
         }
     }
@@ -64,4 +63,27 @@ fileprivate extension HTTPRequest {
     var retailerId : String? {
         return self.urlVariables["retailerId"]
     }
+}
+
+
+extension Retailer {
+
+    public static func retailerBounce(_ request: HTTPRequest, _ response: HTTPResponse) {
+        
+        let sqlCheck = "SELECT "
+        
+        let terminal = Terminal()
+        
+        // this is where we will check the temrminal ID, retailer and the secret to make sure the terminal is approved.
+        
+        do {
+            try terminal.get(request.session?.userid ?? "")
+//            if user.usertype != .admin {
+//                response.redirect(path: "/")
+//            }
+        } catch {
+            print(error)
+        }
+    }
+ 
 }
