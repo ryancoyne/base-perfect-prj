5f1a7f29f9338444df63f88601b364d5ff26b8d3
Ryan Coyne <ryan@clearcodex.com>
2018-08-23 17:50:43 -0400

    working on forgot password on web

--- Sources/bucket/prj-apis/ConsumerAPI.swift
+++ Sources/bucket/prj-apis/ConsumerAPI.swift
@@ -24,9 +24,59 @@ struct ConsumerAPI {
         static var routes : [[String:Any]] {
             return [
                 ["method":"post", "uri":"/login", "handler":login],
-//                ["method":"get", "uri":"/login", "handler":loginPage],
+                ["method":"get", "uri":"/forgotpassword", "handler":forgotPassword],
+                ["method":"post", "uri":"/forgotpasswordEntered", "handler":forgotPasswordEntered],
             ]
         }
+        
+        public static func forgotPassword(data: [String:Any]) throws -> RequestHandler {
+            return {
+                request, response in
+                
+                response.render(template: "views/forgotpassword")
+                response.completed()
+                
+            }
+        }
+        
+        public static func forgotPasswordEntered(data: [String:Any]) throws -> RequestHandler {
+            return {
+                request, response in
+                
+                // get the email:
+                guard let email = request.param(name: "email") else { return }
+                // Okay see if we can find the account:
+                let find = Account()
+                try? find.find(["email":email])
+                
+                if !find.id.isEmpty {
+                    // okay.  we need to create their pass reset thingy and send an email:
+                    
+                    find.passreset = AccessToken.generate()
+                    
+                    if (try? find.save()).isNotNil {
+                        let h = "<p>To reset your password for your account, please <a href=\"\(baseURL)/verifyAccount/forgotpassword/\(find.passreset)\">click here</a></p>"
+                        
+                        response.render(template: "views/forgotpassword", context: ["msg_body":"We sent a confirmation email to \(email).","msg_title":"Success!"])
+                        response.completed()
+                        
+                        Utility.sendMail(name: find.username, address: email, subject: "Password reset!", html: h, text: "")
+                    } else {
+                        
+                        response.render(template: "views/forgotpassword", context: ["msg_body":"Please try again.","msg_title":"Unknown Error."])
+                        response.completed()
+                        
+                    }
+                    
+                } else {
+                    // Show an error:
+                    response.render(template: "views/forgotpassword", context: ["msg_body":"We had an issue looking up this email.","msg_title":"Forgot Password Error"])
+                    response.completed()
+                }
+                
+            }
+        }
+        
         public static func login(data: [String:Any]) throws -> RequestHandler {
             return {
                 request, response in
