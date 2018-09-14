ea1eb30203899e73ebcace2fb74876d34f5a1c7a
Mike Silvers <mike.silvers@buckettechnologies.com>
2018-09-14 14:57:07 -0400

    Added the icon images to the cashout groups

--- Sources/bucket/prj-models/InitilizationData.swift
+++ Sources/bucket/prj-models/InitilizationData.swift
@@ -173,12 +173,31 @@ final class InitializeData {
         var schema = usa.code_alpha_2!.lowercased()
         
         var checkuser = "INSERT INTO \(schema).\(tbl.table()) "
-        checkuser.append("(created, createdby, group_name,description, country_id, picture_url, display_order, display, option_layout, long_description, detail_disbursement_reasons) ")
+        checkuser.append("(created, createdby, group_name,description, country_id, picture_url, icon_url, display_order, display, option_layout, long_description, detail_disbursement_reasons) ")
         checkuser.append(" VALUES ")
-        checkuser.append(" ('\(created_time)','\(CCXDefaultUserValues.user_server)','Prepaid Card','This card allows users to purchase anything using the giftcard.', \(usa.id!),'', 1, true, 'double', 'Cha-ching!  Shop at your favorite retailers with your prepaid card.  Note that these are virtual cards and cannot be used in physical stores.', \(USDetailDisbursementReasons.closedLoopCard)), ")
-        checkuser.append(" ('\(created_time)','\(CCXDefaultUserValues.user_server)','Gift Card','This card allows users to purchase from specific retailers using the giftcard.', \(usa.id!),'',2, true, 'double', 'Choose from hundreds of gift cards to your favorite stores and restaurants.  Note that these are virtual gift cards and some merchants may not accept them in stores.  Please check with the merchant.', \(USDetailDisbursementReasons.openLoopCard)), ")
-        checkuser.append(" ('\(created_time)','\(CCXDefaultUserValues.user_server)','Donate','This allows the user to donate to a specific cause.', \(usa.id!),'',3, true, 'single', 'Choose from hundreds of charities and give back to your community.', \(USDetailDisbursementReasons.donation)), ")
-        checkuser.append(" ('\(created_time)','\(CCXDefaultUserValues.user_server)','Bucket Coin','This is the cryptocurrency for the Bucket users.', \(usa.id!),'',4, true, 'double', null, \(USDetailDisbursementReasons.crypto))")
+        var pic = EnvironmentVariables.sharedInstance.ImageBaseURL!
+        pic.append("/")
+        var pic_icon = EnvironmentVariables.sharedInstance.ImageBaseURL!
+        pic_icon.append("/")
+        checkuser.append(" ('\(created_time)','\(CCXDefaultUserValues.user_server)','Prepaid Card','This card allows users to purchase anything using the giftcard.', \(usa.id!),'\(pic)','\(pic_icon)', 1, true, 'double', 'Cha-ching!  Shop at your favorite retailers with your prepaid card.  Note that these are virtual cards and cannot be used in physical stores.', \(USDetailDisbursementReasons.closedLoopCard)), ")
+        
+        pic = EnvironmentVariables.sharedInstance.ImageBaseURL!
+        pic.append("/")
+        pic_icon = EnvironmentVariables.sharedInstance.ImageBaseURL!
+        pic_icon.append("/")
+        checkuser.append(" ('\(created_time)','\(CCXDefaultUserValues.user_server)','Gift Card','This card allows users to purchase from specific retailers using the giftcard.', \(usa.id!),'\(pic)','\(pic_icon)',2, true, 'double', 'Choose from hundreds of gift cards to your favorite stores and restaurants.  Note that these are virtual gift cards and some merchants may not accept them in stores.  Please check with the merchant.', \(USDetailDisbursementReasons.openLoopCard)), ")
+        
+        pic = EnvironmentVariables.sharedInstance.ImageBaseURL!
+        pic.append("/")
+        pic_icon = EnvironmentVariables.sharedInstance.ImageBaseURL!
+        pic_icon.append("/")
+        checkuser.append(" ('\(created_time)','\(CCXDefaultUserValues.user_server)','Donate','This allows the user to donate to a specific cause.', \(usa.id!),'\(pic)','\(pic_icon)',3, true, 'single', 'Choose from hundreds of charities and give back to your community.', \(USDetailDisbursementReasons.donation)), ")
+        
+        pic = EnvironmentVariables.sharedInstance.ImageBaseURL!
+        pic.append("/")
+        pic_icon = EnvironmentVariables.sharedInstance.ImageBaseURL!
+        pic_icon.append("/")
+        checkuser.append(" ('\(created_time)','\(CCXDefaultUserValues.user_server)','Bucket Coin','This is the cryptocurrency for the Bucket users.', \(usa.id!),'\(pic)','\(pic_icon)',4, true, 'double', null, \(USDetailDisbursementReasons.crypto))")
 
         print("Adding cashout groups for \(schema): \(checkuser)")
         _ = try? tbl.sqlRows(checkuser, params: [])
@@ -186,11 +205,26 @@ final class InitializeData {
         schema = singapore.code_alpha_2!.lowercased()
         
         checkuser = "INSERT INTO \(schema).\(tbl.table()) "
-        checkuser.append("(created, createdby, group_name,description, country_id, picture_url, display_order, display, option_layout, long_description) ")
+        checkuser.append("(created, createdby, group_name,description, country_id, picture_url,icon_url, display_order, display, option_layout, long_description) ")
         checkuser.append(" VALUES ")
-        checkuser.append(" ('\(created_time)','\(CCXDefaultUserValues.user_server)','TopUp','', \(singapore.id!),'',1, true, 'double', null),")
-        checkuser.append(" ('\(created_time)','\(CCXDefaultUserValues.user_server)','Bucket Coin','This is the cryptocurrency for the Bucket users.', \(singapore.id!),'',2, true, 'double', null), ")
-        checkuser.append(" ('\(created_time)','\(CCXDefaultUserValues.user_server)','Donate','', \(singapore.id!),'',3, true, 'single', 'Choose from hundreds of charities and give back to your community.')")
+        
+        pic = EnvironmentVariables.sharedInstance.ImageBaseURL!
+        pic.append("/")
+        pic_icon = EnvironmentVariables.sharedInstance.ImageBaseURL!
+        pic_icon.append("/")
+        checkuser.append(" ('\(created_time)','\(CCXDefaultUserValues.user_server)','TopUp','', \(singapore.id!),'\(pic)','\(pic_icon)',1, true, 'double', null),")
+        
+        pic = EnvironmentVariables.sharedInstance.ImageBaseURL!
+        pic.append("/")
+        pic_icon = EnvironmentVariables.sharedInstance.ImageBaseURL!
+        pic_icon.append("/")
+        checkuser.append(" ('\(created_time)','\(CCXDefaultUserValues.user_server)','Bucket Coin','This is the cryptocurrency for the Bucket users.', \(singapore.id!),'\(pic)','\(pic_icon)',2, true, 'double', null), ")
+        
+        pic = EnvironmentVariables.sharedInstance.ImageBaseURL!
+        pic.append("/")
+        pic_icon = EnvironmentVariables.sharedInstance.ImageBaseURL!
+        pic_icon.append("/")
+        checkuser.append(" ('\(created_time)','\(CCXDefaultUserValues.user_server)','Donate','', \(singapore.id!),'\(pic)','\(pic_icon)',3, true, 'single', 'Choose from hundreds of charities and give back to your community.')")
 
         print("Adding cashout groups for \(schema): \(checkuser)")
         _ = try? tbl.sqlRows(checkuser, params: [])
