184f332783536737b56987b0362b523c5be1252d
Ryan <ryan@buckettechnologies.com>
2018-09-20 15:03:55 -0400

    Updating initialization data

--- Sources/bucket/prj-models/InitilizationData.swift
+++ Sources/bucket/prj-models/InitilizationData.swift
@@ -270,6 +270,8 @@ final class InitializeData {
                 ussql.append("'visa.com', ")
                 ussql.append("'The VISA prepaid card may be used anywhere online!', ")
                 ussql.append("'Come join the VISA family and use this card worldwide!', ")
+                // Confirmation Description:
+                ussql.append(" '', ")
                 ussql.append("'\(largeImage)','\(smallImage)', null, ")
                 ussql.append("50.00,0,true, '{ \"vendor\" : \"FIS\" }'),")
                 // card 2
@@ -281,6 +283,8 @@ final class InitializeData {
                 ussql.append("'mastercard.com', ")
                 ussql.append("'The MasterCard prepaid card may be used anywhere online!', ")
                 ussql.append("'Come join the MasterCard family and use this card worldwide!', ")
+                // Confirmation Description:
+                ussql.append(" '', ")
                 ussql.append("'\(largeImage)','\(smallImage)', null, ")
                 ussql.append("50.00,0,true, '{ \"vendor\" : \"FIS\" }'),")
                 break
@@ -296,6 +300,8 @@ final class InitializeData {
                 ussql.append("'amazon.com',")
                 ussql.append("'Amazon holds the world at your fingertips.',")
                 ussql.append("'Amazon is the largest onine retailer in the world.  Use your Amazon card here!',")
+                // Confirmation Description:
+                ussql.append(" 'Will be sent to {email}\non an Amazon Gift Card', ")
                 ussql.append("'\(largeImage)','\(smallImage)', null,")
                 ussql.append("50.00,0,true, '{\"vendor\" : \"OmniCard\", \"vendorId\":810}'),")
                 
@@ -308,6 +314,8 @@ final class InitializeData {
                 ussql.append("'target.com',")
                 ussql.append("'Target is the store with the big red dot.',")
                 ussql.append("'Target is a department store with a little bit of everything!',")
+                // Confirmation Description:
+                ussql.append(" 'Will be sent to {email}\non a Target Gift Card', ")
                 ussql.append("'\(largeImage)','\(smallImage)', null,")
                 ussql.append("50.00,0,true, '{\"vendor\" : \"OmniCard\", \"vendorId\":678}'),")
                 
@@ -320,6 +328,8 @@ final class InitializeData {
                 ussql.append("'thinkgeek.com',")
                 ussql.append("'Fun things for geeks',")
                 ussql.append("'Want to buy an amazing gift for a geek?  Get it here!',")
+                // Confirmation Description:
+                ussql.append(" 'Will be sent to {email}\non a ThinkGeek Gift Card', ")
                 ussql.append("'\(largeImage)','\(smallImage)', null,")
                 ussql.append("50.00,0,true, '{\"vendor\" : \"OmniCard\", \"vendorId\":933}'),")
                 
@@ -332,6 +342,8 @@ final class InitializeData {
                 ussql.append("'wholefoods.com',")
                 ussql.append("'Want good food?',")
                 ussql.append("'Whole Foods provides a wholistic approach to feed.',")
+                // Confirmation Description:
+                ussql.append(" 'Will be sent to {email}\non a Whole Foods Gift Card', ")
                 ussql.append("'\(largeImage)','\(smallImage)', null,")
                 ussql.append("50.00,0,true, '{\"vendor\" : \"OmniCard\", \"vendorId\":768}'),")
                 
@@ -344,6 +356,8 @@ final class InitializeData {
                 ussql.append("'thecheesecakefactory.com',")
                 ussql.append("'Want Cheesecake?',")
                 ussql.append("'The Cheesecake Factory creates cheesecake masterpieces!',")
+                // Confirmation Description:
+                ussql.append(" 'Will be sent to {email}\non a The Cheesecake Factory Gift Card', ")
                 ussql.append("'\(largeImage)','\(smallImage)', null,")
                 ussql.append("50.00,0,true, '{\"vendor\" : \"OmniCard\", \"vendorId\":1044}'),")
                 
@@ -356,6 +370,8 @@ final class InitializeData {
                 ussql.append("'rubytuesday.com',")
                 ussql.append("'Eat here!',")
                 ussql.append("'Hungry on Tuesday?  Come here and join us for dinner.',")
+                // Confirmation Description:
+                ussql.append(" 'Will be sent to {email}\non a Ruby Tuesday Gift Card', ")
                 ussql.append("'\(largeImage)','\(smallImage)', null,")
                 ussql.append("50.00,0,true, '{\"vendor\" : \"OmniCard\", \"vendorId\":924}'),")
                 
@@ -383,6 +399,8 @@ final class InitializeData {
                 ussql.append("'heifer.org',")
                 ussql.append("'Heifer International works with communities to create income, empower women, care for the Earth, and ultimately end world hunger and poverty',")
                 ussql.append("'Heifer International’s mission is to end hunger and poverty while caring for the Earth. For more than 70 years, we have provided livestock and environmentally sound agricultural training to improve the lives of those who struggle daily for reliable sources of food and income. We currently work in 25 countries, including the United States, to help families and communities become self-reliant.',")
+                // Confirmation Description:
+                ussql.append(" 'Will be donated to:\nHeifer International', ")
                 ussql.append("'\(largeImgUrl)', '\(smImgUrl)', '\(iconImgUrl)',")
                 ussql.append("50.00,0,true, '{ \"vendor\" : \"PureCharity\" }'),")
                 // card 2
@@ -395,6 +413,8 @@ final class InitializeData {
                 ussql.append("'fayettevillekids.org',")
                 ussql.append("'We are a resource for families to improve the quality of their lives through the development of youth in a safe environment.',")
                 ussql.append("'The Donald W. Reynolds Boys & Girls Club is a non-profit 501 (C)(3) organization currently serving over 10,000 community members per year through memberships, special events, facility reservations and drop-in business. Formerly known as the Fayetteville Youth Center, the Club over 75 years of service to youth and families in the local community.',")
+                // Confirmation Description:
+                ussql.append(" 'Will be donated to:\nBoys and Girls Club of Fayetteville', ")
                 ussql.append("'\(largeImgUrl)','\(smImgUrl)', '\(iconImgUrl)',")
                 ussql.append("50.00,0,true, '{ \"vendor\" : \"PureCharity\" }'),")
                 
@@ -408,6 +428,8 @@ final class InitializeData {
                 ussql.append("'nwacasa.org',")
                 ussql.append("'Court Appointed Special Advocates of Northwest Arkansas provides compassionate volunteers who advocate for abused and neglected children.',")
                 ussql.append("'In essence, Court Appointed Special Advocates of Northwest Arkansas recruits, trains, and supervises volunteers who provide one-on-one advocacy for abused children and their families. Our CASA volunteers assist children by stabilizing their lives and moving them through the foster care system. CASAs are responsible for assessing the needs of the victims, making referrals for services (counseling, speech/occupational/physical therapy, educational interventions, medical and dental care) and making sure the emotional and physical needs of children are being met while in care.',")
+                // Confirmation Description:
+                ussql.append(" 'Will be donated to:\nCASA of Northwest Arkansas', ")
                 ussql.append("'\(largeImgUrl)','\(smImgUrl)', '\(iconImgUrl)',")
                 ussql.append("50.00,0,true, '{ \"vendor\" : \"PureCharity\" }'),")
                 
@@ -421,6 +443,8 @@ final class InitializeData {
                 ussql.append("'dfsnwa.org',")
                 ussql.append("'Dress for Success Northwest Arkansas helps empower women toward economic independence by providing a network of support, professional attire, and programs that help them thrive in work and in life.',")
                 ussql.append("'Dress for Success Northwest Arkansas helps empower women toward economic independence by providing a network of support, professional attire, and programs that help her secure employment, retain her job, grow her career, provide for her family and improve their lives.  We offer long-lasting solutions the enable women to break the cycle of poverty.',")
+                // Confirmation Description:
+                ussql.append(" 'Will be donated to:\nDress for Success of NW Arkansas', ")
                 ussql.append("'\(largeImgUrl)','\(smImgUrl)', '\(iconImgUrl)',")
                 ussql.append("50.00,0,true, '{ \"vendor\" : \"PureCharity\" }'),")
                 
@@ -434,6 +458,8 @@ final class InitializeData {
                 ussql.append("'kuaf.com',")
                 ussql.append("'KUAF 91.3 FM is northwest and Western Arkansas’ NPR affiliate and listener-supported radio station. We serve a 14-county area, with a population of 600,000, with NPR news programming.',")
                 ussql.append("'KUAF is northwest and western Arkansas’ NPR affiliate and listener-supported radio station. Owned by the University of Arkansas, KUAF has grown from “The 10-Watt Wonder” in 1973 to the 100,000-watt station it is today. We’ve been this area’s NPR affiliate for 33 years and have changed and grown along with the region, while also holding tight to our roots. We serve a 14-county area, with a population of 600,000, with NPR news programming - news that is highly-researched and vetted and is presented with insight and civil conversation. In today’s political climate, this kind of news programming is more important now than ever before. But more than just news, KUAF is unique in our area as the sole public radio station – and is unique in the breadth and variety of local programming offered. KUAF produces or airs 10 locally-produced programs! Very few other stations of this size produce this much local content – from the Community Spotlight Series with Pete Hartman to local news casts every morning to our daily, news magazine Ozarks at Large,hosted by Kyle Kellams. With the explosive growth in population of our area, KUAF aims to serve not just as a source for news and entertainment, but also as an anchor and introduction to the culture of our region. From The Pickin’ Post, The Generic Blues Show, and Shades of Jazz, plus the hundreds of local events featured in public service announcements and performances of local favorites in the Firmin-Garner performance studio, KUAF reflects the community it serves. We have heard from many listeners new to the area, that listening to KUAF helped them get a better understanding of their new home – from the local news to cultural coverage to the spirit of philanthropy that makes Northwest Arkansas so outstanding.',")
+                // Confirmation Description:
+                ussql.append(" 'Will be donated to:\nKUAF', ")
                 ussql.append("'\(largeImgUrl)','\(smImgUrl)', '\(iconImgUrl)',")
                 ussql.append("50.00,0,true, '{ \"vendor\" : \"PureCharity\" }'),")
                 
@@ -447,6 +473,8 @@ final class InitializeData {
                 ussql.append("'kendrickfincher.org',")
                 ussql.append("'Mission: Promote proper hydration and prevent heat illness through education and supporting activities. Vision: Expand national awareness and education to save lives.',")
                 ussql.append("'Mission: Promote proper hydration and prevent heat illness through  education and supporting activities.  Vision: Expand national awareness and education to save lives.  Education and Supporting Activities: Be Smart. BeeHydrated! - Presentations to school aged children on the importance of proper hydration Beat the Heat - Presentations to athletes, coaches and parents on proper hydration and heat illness prevention Distribution of squeeze bottles and educational pamphlets to support our educational presentations Representation at health fairs to educate the public about our mission and activities Community involvement in support of our mission by providing “cool huts”—misting tents with free ice water—at various outdoor public events. Annual youth run in Rogers, AR, to reinforce our mission and help children and the community learn about the importance of proper hydration and physical fitness in a fun environment. Partnerships with other sports injury prevention and wellness organizations Web presence that allows for regional and national reach Developing education programs for all ages including senior adults and industrial workforce',")
+                // Confirmation Description:
+                ussql.append(" 'Will be donated to:\nKendrick Fincher Foundation', ")
                 ussql.append("'\(largeImgUrl)','\(smImgUrl)', '\(iconImgUrl)',")
                 ussql.append("50.00,0,true, '{ \"vendor\" : \"PureCharity\" }'),")
 
@@ -457,6 +485,8 @@ final class InitializeData {
                 ussql.append("'https://buckettechnologies.com',")
                 ussql.append("'BUX: Our Crypto Coin',")
                 ussql.append("'Jump into the Crypto world!  Buy you BUX today.',")
+                // Confirmation Description:
+                ussql.append(" null, ")
                 ussql.append(" null, null, null,")
                 ussql.append("50.00,0,true, '{ }'),")
                 break
@@ -473,7 +503,7 @@ final class InitializeData {
         
         var checkuser = "INSERT INTO \(schema).\(tbl.table()) "
         checkuser.append("(created, createdby, group_id, form_id, display_order, ")
-        checkuser.append("name, website, description, long_description, picture_url, sm_picture_url, icon_url, ")
+        checkuser.append("name, website, description, long_description, confirmation_description, picture_url, sm_picture_url, icon_url, ")
         checkuser.append("minimum, maximum, display, vendor_detail) ")
         checkuser.append(" VALUES ")
         checkuser.append(ussql)
@@ -514,9 +544,11 @@ final class InitializeData {
                 ussql.append("'Bucket Coin',")
                 ussql.append("'https://buckettechnologies.com',")
                 ussql.append("'BUX: Our Crypto Coin',")
-                ussql.append("'Jump into the Crypto world!  Buy you BUX today.',")
-                ussql.append("'','',")
-                ussql.append("50.00,0,true),")
+                ussql.append("'Jump into the Crypto world!  Buy you BUX today.', ")
+                // Confirmation Description:
+                ussql.append("null, ")
+                ussql.append("null, null, ")
+                ussql.append("50.00, 0, true),")
                 break
             default:
                 break
@@ -529,7 +561,7 @@ final class InitializeData {
         
         checkuser = "INSERT INTO \(schema).\(tbl.table()) "
         checkuser.append("(created, createdby, group_id, form_id, display_order, ")
-        checkuser.append("name, website, description, long_description, picture_url, sm_picture_url, ")
+        checkuser.append("name, website, description, long_description, confirmation_description, picture_url, sm_picture_url, ")
         checkuser.append("minimum, maximum, display) ")
         checkuser.append(" VALUES ")
         checkuser.append(ussql)
