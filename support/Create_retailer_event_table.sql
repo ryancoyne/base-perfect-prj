c0680f573f54f1d52efe4c330f775c4b8cadf5a6
Mike Silvers <mike.silvers@buckettechnologies.com>
2018-11-16 15:09:30 -0500

    Working on the new functions for totals and reports

--- support/Create_retailer_event_table.sql
+++ support/Create_retailer_event_table.sql
@@ -1,3 +1,12 @@
+CREATE SEQUENCE us.retailer_event_id_seq INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1;
+CREATE SEQUENCE sg.retailer_event_id_seq INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1;
+
+ALTER SEQUENCE us.retailer_user_id_seq OWNER TO bucket;
+ALTER SEQUENCE sg.retailer_user_id_seq OWNER TO bucket;
+
 CREATE TABLE IF NOT EXISTS us.retailer_event ( id integer NOT NULL DEFAULT nextval('us.retailer_event_id_seq'::regclass), created integer NOT NULL DEFAULT 0, createdby text COLLATE pg_catalog.default, modified integer NOT NULL DEFAULT 0, modifiedby text COLLATE pg_catalog.default, deleted integer NOT NULL DEFAULT 0, deletedby text COLLATE pg_catalog.default, retailer_id int default 0, event_name text COLLATE pg_catalog.default, event_message int default 0, start_date int default 0, end_date int default 0, CONSTRAINT retailer_event_pkey PRIMARY KEY (id) );
 
 CREATE TABLE IF NOT EXISTS sg.retailer_event ( id integer NOT NULL DEFAULT nextval('sg.retailer_event_id_seq'::regclass), created integer NOT NULL DEFAULT 0, createdby text COLLATE pg_catalog.default, modified integer NOT NULL DEFAULT 0, modifiedby text COLLATE pg_catalog.default, deleted integer NOT NULL DEFAULT 0, deletedby text COLLATE pg_catalog.default, retailer_id int default 0, event_name text COLLATE pg_catalog.default, event_message int default 0, start_date int default 0, end_date int default 0, CONSTRAINT retailer_event_pkey PRIMARY KEY (id) );
+
+ALTER TABLE us.retailer_event OWNER TO bucket;
+ALTER TABLE sg.retailer_event OWNER TO bucket;
--- support/functions.sql
+++ support/functions.sql
@@ -1,41 +1,3 @@
-CREATE OR REPLACE FUNCTION us.getTransactionReport(fromDate int, toDate int, retailerId int, terminalId int=0, retailerUserId int=0, offsetBy int=0, limitBy int=200)
-RETURNS TABLE (id int, created int, amount numeric, total_amount numeric, client_location text,
-  client_transaction_id text, terminal_id int, disputed int, disputedby text, customer_code text, redeemed int)
-AS $function$
-
-BEGIN
-
-  IF terminalId = 0 AND retailerUserId = 0 THEN
-
-    RETURN QUERY
-      SELECT ct.id, ct.created, ct.amount, ct.total_amount, ct.client_location, ct.client_transaction_id, ct.terminal_id, ct.disputed, ct.disputedby, ct.customer_code, ct.redeemed
-      FROM us.code_transaction AS ct
-      WHERE (ct.created BETWEEN fromDate AND toDate) AND (ct.retailer_id = retailerId)
-      UNION
-      SELECT cth.id, cth.created, cth.amount, cth.total_amount, cth.client_location, cth.client_transaction_id, cth.terminal_id, cth.disputed, cth.disputedby, cth.customer_code, cth.redeemed
-      FROM us.code_transaction_history AS cth
-      WHERE (cth.created BETWEEN fromDate AND toDate) AND (cth.retailer_id = retailerId)
-      ORDER BY created DESC
-      OFFSET offsetBy LIMIT limitBy;
-  ELSE
-
-    RETURN QUERY
-      SELECT ct.id, ct.created, ct.amount, ct.total_amount, ct.client_location, ct.client_transaction_id, ct.terminal_id, ct.disputed, ct.disputedby, ct.customer_code, ct.redeemed
-      FROM us.code_transaction AS ct
-      WHERE (ct.created BETWEEN fromDate AND toDate) AND (ct.retailer_id = retailerId) AND (ct.terminal_id = terminalId)
-      UNION
-      SELECT cth.id, cth.created, cth.amount, cth.total_amount, cth.client_location, cth.client_transaction_id, cth.terminal_id, cth.disputed, cth.disputedby, cth.customer_code, cth.redeemed
-      FROM us.code_transaction_history AS cth
-      WHERE (cth.created BETWEEN fromDate AND toDate) AND (cth.retailer_id = retailerId) AND (cth.terminal_id = terminalId)
-      ORDER BY created DESC
-      OFFSET offsetBy LIMIT limitBy;
-  END IF;
-
-END $function$
-
-LANGUAGE plpgsql;
-
-THE NEW FUNCTION:
 
 CREATE OR REPLACE FUNCTION us.getTransactionReport(fromDate int, toDate int, retailerId int, terminalId int=0, retailerUserId int=0, offsetBy int=0, limitBy int=200)
 RETURNS TABLE (id int, created int, amount numeric, total_amount numeric, client_location text,
@@ -56,6 +18,7 @@ FROM us.code_transaction_history AS cth
 WHERE (cth.created BETWEEN fromDate AND toDate) AND (cth.retailer_id = retailerId)
 ORDER BY created DESC
 OFFSET offsetBy LIMIT limitBy;
+
 ELSIF (retailerUserId = 0) AND (terminalId > 0) THEN
 
 RETURN QUERY
@@ -68,6 +31,7 @@ FROM us.code_transaction_history AS cth
 WHERE (cth.created BETWEEN fromDate AND toDate) AND (cth.retailer_id = retailerId) AND (cth.terminal_id = terminalId)
 ORDER BY created DESC
 OFFSET offsetBy LIMIT limitBy;
+
 ELSIF (retailerUserId > 0) AND (terminalId > 0) THEN
 
 RETURN QUERY
@@ -80,6 +44,7 @@ FROM us.code_transaction_history AS cth
 WHERE (cth.created BETWEEN fromDate AND toDate) AND (cth.retailer_id = retailerId) AND (cth.terminal_id = terminalId) AND (cth.retailer_user_id = retailerUserId)
 ORDER BY created DESC
 OFFSET offsetBy LIMIT limitBy;
+
 ELSE
 
 RETURN QUERY
@@ -97,3 +62,86 @@ END IF;
 END $function$
 
 LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION us.getTransactionReportTotals(fromDate bigint, toDate bigint, retailerId int, terminalId int=0, retailerUserId int=0, offsetBy int=0, limitBy int=200, OUT total_count bigint, OUT total_value numeric)
+AS
+$$
+
+DECLARE
+var_r record;
+
+BEGIN
+
+total_count := 0;
+total_value = 0;
+
+IF terminalId = 0 AND retailerUserId = 0 THEN
+
+FOR var_r IN(
+
+SELECT ct.redeemed, count(*) over () as the_count, sum(ct.amount) over () as the_value
+FROM us.code_transaction AS ct
+WHERE (ct.created BETWEEN fromDate AND toDate) AND (ct.retailer_id = retailerId)
+UNION
+SELECT cth.redeemed, count(*) over () as the_count, sum(cth.amount) over () as the_value
+FROM us.code_transaction_history AS cth
+WHERE (cth.created BETWEEN fromDate AND toDate) AND (cth.retailer_id = retailerId))
+LOOP
+total_count := total_count + var_r.the_count;
+total_value := total_value + var_r.the_value;
+END LOOP;
+RETURN;
+
+ELSIF (retailerUserId = 0) AND (terminalId > 0) THEN
+
+FOR var_r IN(
+
+SELECT ct.redeemed, count(*) over () as the_count, sum(ct.amount) over () as the_value
+FROM us.code_transaction AS ct
+WHERE (ct.created BETWEEN fromDate AND toDate) AND (ct.retailer_id = retailerId) AND (ct.terminal_id = terminalId)
+UNION
+SELECT cth.redeemed, count(*) over () as the_count, sum(cth.amount) over () as the_value
+FROM us.code_transaction_history AS cth
+WHERE (cth.created BETWEEN fromDate AND toDate) AND (cth.retailer_id = retailerId) AND (cth.terminal_id = terminalId))
+LOOP
+total_count := total_count + var_r.the_count;
+total_value := total_value + var_r.the_value;
+END LOOP;
+RETURN;
+
+ELSIF (retailerUserId > 0) AND (terminalId > 0) THEN
+
+FOR var_r IN(
+SELECT ct.redeemed, count(*) over () as the_count, sum(ct.amount) over () as the_value
+FROM us.code_transaction AS ct
+WHERE (ct.created BETWEEN fromDate AND toDate) AND (ct.retailer_id = retailerId) AND (ct.terminal_id = terminalId) AND (ct.retailer_user_id = retailerUserId)
+UNION
+SELECT cth.redeemed, count(*) over () as the_count, sum(cth.amount) over () as the_value
+FROM us.code_transaction_history AS cth
+WHERE (cth.created BETWEEN fromDate AND toDate) AND (cth.retailer_id = retailerId) AND (cth.terminal_id = terminalId) AND (cth.retailer_user_id = retailerUserId))
+LOOP
+total_count := total_count + var_r.the_count;
+total_value := total_value + var_r.the_value;
+END LOOP;
+RETURN;
+
+ELSE
+
+FOR var_r IN(
+SELECT ct.redeemed, count(*) over () as the_count, sum(ct.amount) over () as the_value
+FROM us.code_transaction AS ct
+WHERE (ct.created BETWEEN fromDate AND toDate) AND (ct.retailer_id = retailerId) AND (ct.retailer_user_id = retailerUserId)
+UNION
+SELECT cth.redeemed, count(*) over () as the_count, sum(cth.amount) over () as the_value
+FROM us.code_transaction_history AS cth
+WHERE (cth.created BETWEEN fromDate AND toDate) AND (cth.retailer_id = retailerId) AND (cth.retailer_user_id = retailerUserId))
+LOOP
+total_count := total_count + var_r.the_count;
+total_value := total_value + var_r.the_value;
+END LOOP;
+RETURN;
+END IF;
+END;
+$$ LANGUAGE plpgsql;
+
