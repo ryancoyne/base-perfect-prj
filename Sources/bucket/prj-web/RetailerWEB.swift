6729095caa005289d3849a5976b4899b463db632
Mike Silvers <mike.silvers@buckettechnologies.com>
2018-11-19 01:44:45 -0500

    Starting the retailer add views

--- Sources/bucket/prj-web/RetailerWEB.swift
+++ Sources/bucket/prj-web/RetailerWEB.swift
@@ -25,8 +25,9 @@ struct RetailerWEB {
         static var routes : [[String:Any]] {
             return [
 //                ["method":"get", "uri":"/retailer", "handler":retailerterminalindex],
-                ["method":"get", "uri":"/retailer/{countryId}", "handler":retailerindex],
-                ["method":"get", "uri":"/retailer/{countryId}/{retailerId}", "handler":retailerdetail],
+                ["method":"get", "uri":"/retailer/{countryId}", "handler":retailer_index],
+                ["method":"get", "uri":"/retailer/{countryId}/add", "handler":retailer_add],
+                ["method":"get", "uri":"/retailer/{countryId}/{retailerId}", "handler":retailer_detail],
 //                ["method":"get", "uri":"/retailer/{countryId}/{retailerId}/terminal/add", "handler":retailer_add_terminal],
 //                ["method":"get", "uri":"/retailer/{countryId}/{retailerId}/terminal/{terminalId}", "handler":retailer_terminal],
 //                ["method":"get", "uri":"/retailer/{countryId}/{retailerId}/address/{addressId}", "handler":retailer_address],
@@ -36,99 +37,9 @@ struct RetailerWEB {
         
         //MARK: --
         //MARK: Retailer Terminal list page
-        struct retailerterminalindexHelper: MustachePageHandler {
-            
-            var values: MustacheEvaluationContext.MapType = [:]
-            
-            func extendValuesForResponse(context contxt: MustacheWebEvaluationContext, collector: MustacheEvaluationOutputCollector) {
-            
-                contxt.extendValues(with: values)
-
-                do {
-                    try contxt.requestCompleted(withCollector: collector)
-                } catch {
-                    let response = contxt.webResponse
-                    response.appendBody(string: "\(error)")
-                    .completed(status: .internalServerError)
-                }
-
-            }
-            
-        }
-        
-        public static func retailerterminalindex(data: [String:Any]) throws -> RequestHandler {
-            return {
-                request, response in
-                
-                // check for the security token - this is the token that shows the request is coming from CloudFront and not outside
-                guard request.SecurityCheck() else { return response.badSecurityToken }
-                
-                // check to see if the user is permitted to these retailer pages
-                let user = request.account
-                if user.isNil { return }
-
-                // lets pull out the retailers list (if they have any)
-                guard let retailer_dict:[String:Any] = user?.detail["retailers"] as? [String : Any] else {
-                    response.render(template: "views/retailer.index")
-                    response.completed()
-                    return
-                }
-
-                var values: MustacheEvaluationContext.MapType = [:]
-                
-                var retailers:[String:[[String:Any]]] = [:]
-                
-                // lets get the retailer information and the unassigned terminals
-                for (key,value) in retailer_dict {
-                    
-                    var ret_array:[[String:Any]] = []
-                    
-                    let schema = key.lowercased()
-                    
-                    // get the retailers together
-                    for i in (value as! [Int]) {
-                        let r = RetailerAll()
-                        r.get(schema, i)
-                        r.country_code = schema
-                        r.country_id = Country.idWith(schema)
-                        ret_array.append(r.asDictionary())
-                    }
-                    
-                    if ret_array.count > 0 {
-                        ret_array.append(["country_code":key])
-                        if let c_id = Country.idWith(key) {
-                            ret_array.append(["country_id":c_id])
-                        }
-                        retailers[schema] = ret_array
-                    }
-                    
-                }
-                
-                // lets add the array of the countries for this retaiuler so we know which ones to address on the page
-                var countries:[String] = []
-                for (key, _) in retailer_dict {
-                    countries.append(key)
-                }
-                
-                if !countries.isEmpty {
-                    // provides the list of countries the user has assigned to thier user profile
-                    values["countries"] = countries
-                }
-                
-                // only send back the retailers if there are any retailers
-                if retailers.count > 0 { values["retailers"] = retailers }
-
-                print(values)
-                
-                mustacheRequest(request: request,
-                                response: response,
-                                handler: retailerterminalindexHelper(values: values),
-                                templatePath: "\(request.documentRoot)/views/retailer.index.mustache")
-            }
-        }
 
         //MARK: --
-        public static func retailerindex(data: [String:Any]) throws -> RequestHandler {
+        public static func retailer_index(data: [String:Any]) throws -> RequestHandler {
             return {
                 request, response in
                 
@@ -214,7 +125,7 @@ struct RetailerWEB {
             }
         }
         
-        public static func retailerdetail(data: [String:Any]) throws -> RequestHandler {
+        public static func retailer_detail(data: [String:Any]) throws -> RequestHandler {
             return {
                 request, response in
                 
@@ -313,150 +224,54 @@ struct RetailerWEB {
                 
             }
         }
-        
-        public static func retailerlocations(data: [String:Any]) throws -> RequestHandler {
-            return {
-                request, response in
 
-                // check for the security token - this is the token that shows the request is coming from CloudFront and not outside
-                guard request.SecurityCheck() else { response.badSecurityToken; return }
-
-                var template = "views/msg" // where it goes to after
-                var context: [String : Any] = ["title": "Bucket Technologies", "subtitle":"Goodbye coins, Hello Change"]
-
-                // check for the security token - this is the token that shows the request is coming from CloudFront and not outside
-                var problem:String
-                if !request.SecurityCheck() {
-                    problem = response.badSecurityTokenWeb
-                    context["msg_title"] = "Login Error."
-                    context["msg_body"] = problem
-                    template = "views/msg"
-                    response.render(template: template, context: context)
-                    response.completed()
-                    return
-                }
-
-                if let i = request.session?.userid, !i.isEmpty { response.redirect(path: "/") }
-                context["csrfToken"] = request.session?.data["csrf"] as? String ?? ""
-                
-                if let email = request.param(name: "email").stringValue, !email.isEmpty,
-                    let password = request.param(name: "password").stringValue, !password.isEmpty {
-                    do {
-                        let account = try Account.loginWithEmail(email, password)
-                        request.session?.userid = account.id
-                        context["msg_title"] = "Login Successful."
-                        context["msg_body"] = ""
-                        response.redirect(path: "/")
-                    } catch {
-                        context["msg_title"] = "Login Error."
-                        context["msg_body"] = "Email or password incorrect"
-                        template = "views/login"
-                    }
-                } else {
-                    context["msg_title"] = "Login Error."
-                    context["msg_body"] = "Email or password not supplied"
-                    template = "views/login"
-                }
-                response.render(template: template, context: context)
-                response.completed()
-            }
-        }
-        
-        public static func retailerterminals(data: [String:Any]) throws -> RequestHandler {
+        public static func retailer_add(data: [String:Any]) throws -> RequestHandler {
             return {
                 request, response in
                 
                 // check for the security token - this is the token that shows the request is coming from CloudFront and not outside
-                guard request.SecurityCheck() else { response.badSecurityToken; return }
+                #if !os(macOS)
+                guard request.SecurityCheck() else { return response.badSecurityToken }
+                #endif
                 
-                var template = "views/msg" // where it goes to after
-                var context: [String : Any] = ["title": "Bucket Technologies", "subtitle":"Goodbye coins, Hello Change"]
-                
-                // check for the security token - this is the token that shows the request is coming from CloudFront and not outside
-                var problem:String
-                if !request.SecurityCheck() {
-                    problem = response.badSecurityTokenWeb
-                    context["msg_title"] = "Login Error."
-                    context["msg_body"] = problem
-                    template = "views/msg"
-                    response.render(template: template, context: context)
-                    response.completed()
+                // grab the country id
+                let country_id = request.countryId
+                var schema = ""
+                if country_id.isNotNil {
+                    schema = Country.getSchema(country_id!)
+                } else {
+                    // there is an error with the country ID
+                    response.unsupportedCountry
                     return
                 }
                 
-                if let i = request.session?.userid, !i.isEmpty { response.redirect(path: "/") }
-                context["csrfToken"] = request.session?.data["csrf"] as? String ?? ""
+                // make sure the user has authority to access the retailer information
+                if country_id.isNil { response.invalidCountryCode; return }
                 
-                if let email = request.param(name: "email").stringValue, !email.isEmpty,
-                    let password = request.param(name: "password").stringValue, !password.isEmpty {
-                    do {
-                        let account = try Account.loginWithEmail(email, password)
-                        request.session?.userid = account.id
-                        context["msg_title"] = "Login Successful."
-                        context["msg_body"] = ""
-                        response.redirect(path: "/")
-                    } catch {
-                        context["msg_title"] = "Login Error."
-                        context["msg_body"] = "Email or password incorrect"
-                        template = "views/login"
-                    }
-                } else {
-                    context["msg_title"] = "Login Error."
-                    context["msg_body"] = "Email or password not supplied"
-                    template = "views/login"
+                #if !os(macOS)
+                // check to see if the user is permitted to these retailer pages
+                let user = request.account
+                if user.isNil { return }
+                if !user!.bounceRetailerAdmin(schema, retailer_id!) {
+                    
                 }
-                response.render(template: template, context: context)
+                #endif
+                
+                var data_return:[String:Any] = [:]
+                
+                data_return["page_retailer_add"] = true
+                data_return["country_id"] = country_id
+                data_return["title_label"] = "Add A Retailer"
+                
+                var ret:[[String:Any]] = []
+                
+                // lets grab the retailer info
+                
+                response.render(template: "views/retailer.add", context: data_return)
                 response.completed()
+                
             }
         }
 
-        //MARK: --
-        public static func retailerterminal(data: [String:Any]) throws -> RequestHandler {
-            return {
-                request, response in
-                
-                // check for the security token - this is the token that shows the request is coming from CloudFront and not outside
-                guard request.SecurityCheck() else { response.badSecurityToken; return }
-                
-                var template = "views/msg" // where it goes to after
-                var context: [String : Any] = ["title": "Bucket Technologies", "subtitle":"Goodbye coins, Hello Change"]
-                
-                // check for the security token - this is the token that shows the request is coming from CloudFront and not outside
-                var problem:String
-                if !request.SecurityCheck() {
-                    problem = response.badSecurityTokenWeb
-                    context["msg_title"] = "Login Error."
-                    context["msg_body"] = problem
-                    template = "views/msg"
-                    response.render(template: template, context: context)
-                    response.completed()
-                    return
-                }
-                
-                if let i = request.session?.userid, !i.isEmpty { response.redirect(path: "/") }
-                context["csrfToken"] = request.session?.data["csrf"] as? String ?? ""
-                
-                if let email = request.param(name: "email").stringValue, !email.isEmpty,
-                    let password = request.param(name: "password").stringValue, !password.isEmpty {
-                    do {
-                        let account = try Account.loginWithEmail(email, password)
-                        request.session?.userid = account.id
-                        context["msg_title"] = "Login Successful."
-                        context["msg_body"] = ""
-                        response.redirect(path: "/")
-                    } catch {
-                        context["msg_title"] = "Login Error."
-                        context["msg_body"] = "Email or password incorrect"
-                        template = "views/login"
-                    }
-                } else {
-                    context["msg_title"] = "Login Error."
-                    context["msg_body"] = "Email or password not supplied"
-                    template = "views/login"
-                }
-                response.render(template: template, context: context)
-                response.completed()
-            }
-        }
     }
 }
