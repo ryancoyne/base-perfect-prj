25d011222f464756bf8649a812f14003b6b11d32
Ryan <ryan@buckettechnologies.com>
2018-11-21 23:29:00 -0500

    Updating the code transaction and history tables SQL doc - added in the stuff that was new to dev and stuff that should be ready to go to staging

--- support/1-Update_Code_transaction_and_history_tables.sql
+++ support/1-Update_Code_transaction_and_history_tables.sql
@@ -1,4 +1,6 @@
 
+-- THIS IS ALL THE NEW STUFF FOR DEV --
+
 ALTER TABLE us.code_transaction ADD COLUMN refunded integer DEFAULT 0;
 ALTER TABLE us.code_transaction ADD COLUMN refundedby text COLLATE pg_catalog.default;
 ALTER TABLE us.code_transaction ADD COLUMN refunded_reason text COLLATE pg_catalog.default;
@@ -14,3 +16,19 @@ ALTER TABLE us.code_transaction_history ADD COLUMN refunded_reason text COLLATE 
 ALTER TABLE sg.code_transaction_history ADD COLUMN refunded integer DEFAULT 0;
 ALTER TABLE sg.code_transaction_history ADD COLUMN refundedby text COLLATE pg_catalog.default;
 ALTER TABLE sg.code_transaction_history ADD COLUMN refunded_reason text COLLATE pg_catalog.default;
+
+
+
+
+
+-- THIS STUFF SHOULD BE READY FOR WHEN IT GOES INTO STAGING --
+
+ALTER TABLE us.code_transaction ADD COLUMN retailer_user_id integer DEFAULT 0;
+ALTER TABLE sg.code_transaction ADD COLUMN retailer_user_id integer DEFAULT 0;
+ALTER TABLE us.code_transaction_history ADD COLUMN retailer_user_id integer DEFAULT 0;
+ALTER TABLE sg.code_transaction_history ADD COLUMN retailer_user_id integer DEFAULT 0;
+
+ALTER TABLE us.code_transaction ADD COLUMN event_id integer DEFAULT 0;
+ALTER TABLE sg.code_transaction ADD COLUMN event_id integer DEFAULT 0;
+ALTER TABLE us.code_transaction_history ADD COLUMN event_id integer DEFAULT 0;
+ALTER TABLE sg.code_transaction_history ADD COLUMN event_id integer DEFAULT 0;
