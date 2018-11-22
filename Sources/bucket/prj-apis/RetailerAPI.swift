bff8ccc1b0aac0e05dd11ad40f2c8a1e34d1c7ad
Ryan <ryan@buckettechnologies.com>
2018-11-21 22:50:34 -0500

    Making one last change to the create transaction API

--- Sources/bucket/prj-apis/RetailerAPI.swift
+++ Sources/bucket/prj-apis/RetailerAPI.swift
@@ -1028,8 +1028,8 @@ struct RetailerAPI {
                     
                     retailerUserId = check.retailerUserId
                     
-                } else if let t = rt.terminal, !t.require_employee_id {
-                    let check = t.checkEmployeeId(request.employeeCode!, schema)
+                } else if let t = rt.terminal, !t.require_employee_id, let employeeCode = request.employeeCode {
+                    let check = t.checkEmployeeId(employeeCode, schema)
                     retailerUserId = check.retailerUserId
                 }
                 
