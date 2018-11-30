c940cd724dff8a32c9304e431cb44556a951bc46
Ryan <ryan@buckettechnologies.com>
2018-11-29 23:12:44 -0500

    Working on file uploads for the cashout groups and then cashout options

--- Sources/bucket/prj-apis/AdminAPI.swift
+++ Sources/bucket/prj-apis/AdminAPI.swift
@@ -48,6 +48,79 @@ struct AdminAPI {
                 
                 // We wont be doing JSON here since we have files to upload for the images:
                 
+                if let id = request.param(name: "id", defaultValue: nil) {
+                    
+                    // We are UPDATING a new group:
+                    let group = CashoutGroup()
+                    _ = try? group.get(id, schema: schema)
+                    
+                    group.country_id = countryId
+                    if let value = request.param(name: "name", defaultValue: nil) {
+                        group.group_name = value
+                    }
+                    if let value = request.param(name: "description", defaultValue: nil) {
+                        group.description = value
+                    }
+                    if let value = request.param(name: "longDescription", defaultValue: nil) {
+                        group.long_description = value
+                    }
+                    if let value = request.param(name: "optionLayout", defaultValue: nil) {
+                        group.option_layout = value
+                    }
+                    if let value = request.param(name: "display", defaultValue: nil).boolValue {
+                        group.display = value
+                    }
+                    if let value = request.param(name: "thresholdAmount", defaultValue: nil).doubleValue {
+                        group.threshold_amount = value
+                    }
+                    if let value = request.param(name: "displayOrder", defaultValue: nil).intValue {
+                        group.display_order = value
+                    }
+                    
+                    _ = try? group.saveWithCustomType(schemaIn: schema, bounce.user?.id)
+                    
+                } else {
+                    // Okay, we are CREATING:
+                    let group = CashoutGroup()
+                    
+                    // First lets make sure they have the images we need:
+                    guard let pictureFiles = request.uploadedFiles else { return response.pictureFilesRequired }
+                    
+                    for pictureFile in pictureFiles {
+                        //  Here we are assuming that this is the 1x image.  We need to create and save the 1x, 1.5x, 2x, 2.5x, 3x, 3.5x, 4x.
+                        // We also need to get an icon, and a background:
+                        switch pictureFile.fieldName {
+                        case "icon":
+                            break
+                        case "background":
+                            break
+                        default: break
+                        }
+                    }
+                    
+                    if let value = request.param(name: "name", defaultValue: nil) {
+                        group.group_name = value
+                    }
+                    if let value = request.param(name: "description", defaultValue: nil) {
+                        group.description = value
+                    }
+                    if let value = request.param(name: "longDescription", defaultValue: nil) {
+                        group.long_description = value
+                    }
+                    if let value = request.param(name: "optionLayout", defaultValue: nil) {
+                        group.option_layout = value
+                    }
+                    if let value = request.param(name: "display", defaultValue: nil).boolValue {
+                        group.display = value
+                    }
+                    if let value = request.param(name: "thresholdAmount", defaultValue: nil).doubleValue {
+                        group.threshold_amount = value
+                    }
+                    if let value = request.param(name: "displayOrder", defaultValue: nil).intValue {
+                        group.display_order = value
+                    }
+                    
+                }
                 
                 do {
                     
@@ -56,64 +129,16 @@ struct AdminAPI {
                     
                     // Okay.. lets see if we are updating or creating:
                     if json.id.isNil {
-                        // We are creating a new group:
-                        let group = CashoutGroup()
                         
-                        group.country_id = countryId
-                        if let value = json["name"].stringValue {
-                            group.group_name = value
-                        }
-                        if let value = json["description"].stringValue {
-                            group.description = value
-                        }
-                        if let value = json["longDescription"].stringValue {
-                            group.long_description = value
-                        }
-                        if let value = json["optionLayout"].stringValue {
-                            group.option_layout = value
-                        }
-                        if let value = json["display"].boolValue {
-                            group.display = value
-                        }
-                        if let value = json["thresholdAmount"].doubleValue {
-                            group.threshold_amount = value
-                        }
-                        if let value = json["displayOrder"].intValue {
-                            group.display_order = value
-                        }
                         
                         // Okay. We need to have images for the cashout groups:
                         
                         
                         // Okay lets save this new group:
-                        _ = try? group.saveWithCustomType(schemaIn: schema, bounce.user?.id)
+                        
                         
                     } else {
-                        // Okay, we are UPDATING:
-                        let group = CashoutGroup()
-                        _ = try? group.get(json.id!, schema: schema)
                         
-                        if let value = json["name"].stringValue {
-                            group.group_name = value
-                        }
-                        if let value = json["description"].stringValue {
-                            group.description = value
-                        }
-                        if let value = json["longDescription"].stringValue {
-                            group.long_description = value
-                        }
-                        if let value = json["optionLayout"].stringValue {
-                            group.option_layout = value
-                        }
-                        if let value = json["display"].boolValue {
-                            group.display = value
-                        }
-                        if let value = json["thresholdAmount"].doubleValue {
-                            group.threshold_amount = value
-                        }
-                        if let value = json["displayOrder"].intValue {
-                            group.display_order = value
-                        }
                         
                         
                         
@@ -217,6 +242,9 @@ fileprivate extension HTTPRequest {
     var groupId : Int {
         return self.urlVariables["groupId"].intValue ?? 0
     }
+    var uploadedFiles : [MimeReader.BodySpec]? {
+        return self.postFileUploads?.filter { $0.file.isNotNil }
+    }
 }
 
 fileprivate extension HTTPResponse {
@@ -229,6 +257,11 @@ fileprivate extension HTTPResponse {
         return try! self.setBody(json: ["errorCode":"GroupAlreadyDeleted", "message": "The group has already been deleted."])
             .setHeader(.contentType, value: "application/json; charset=UTF-8")
             .completed(status: .custom(code: 404, message: "The Group Id does not exist."))
+    }
+    var pictureFilesRequired : Void {
+        return try! self.setBody(json: ["errorCode":"PictureFilesRequired", "message": "You must include picture files to create a new cashout group."])
+            .setHeader(.contentType, value: "application/json; charset=UTF-8")
+            .completed(status: .custom(code: 400, message: "Picture Files Are Required"))
     }
     func deletedGroup(_ id: Int) -> Void {
         return try! self.setBody(json: ["message":"You successfully deleted group id: \(id)."])
