161b76ee0bb8198aa36bb8e8557ab16d3ede7f3a
Ryan <ryan@buckettechnologies.com>
2018-12-14 00:18:38 -0500

    Working on updates to my custom local authentication module - taking out the extended class for the account

--- Package.swift
+++ Package.swift
@@ -8,7 +8,9 @@ let package = Package(
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", versions: Version(3,0,0)..<Version(3,9223372036854775807,9223372036854775807)),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Notifications.git", versions: Version(3,0,0)..<Version(3,9223372036854775807,9223372036854775807)),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", versions: Version(3,0,0)..<Version(3,9223372036854775807,9223372036854775807)),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-LocalAuthentication-PostgreSQL.git", versions: Version(3,0,0)..<Version(3,9223372036854775807,9223372036854775807)),
+        // We will be using my Perfect local auth, as it has some custom modifications to the StORM and PostgresStORM classes.
+//        .Package(url: "https://github.com/PerfectlySoft/Perfect-LocalAuthentication-PostgreSQL.git", versions: Version(3,0,0)..<Version(3,9223372036854775807,9223372036854775807)),
+        .Package(url: "https://github.com/ryancoyne/Perfect-LocalAuthentication-PostgreSQL-ryan.git", versions: Version(3,0,0)..<Version(3,9223372036854775807,9223372036854775807)),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-OAuth2.git", versions: Version(3,0,0)..<Version(3,9223372036854775807,9223372036854775807)),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", versions: Version(3,0,0)..<Version(3,9223372036854775807,9223372036854775807)),
 		.Package(url: "https://github.com/michaelsilvers/SwiftGD.git", versions: Version(1,0,0)..<Version(1,9223372036854775807,9223372036854775807)),
--- Sources/bucket/rm/friends/Friends.swift
+++ Sources/bucket/rm/friends/Friends.swift
@@ -39,7 +39,7 @@ class Friends: PostgresStORM {
     
     //MARK -
     //MARK: initializers
-    override init() {
+    required init() {
         super.init()
     }
     
